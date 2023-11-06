71f0bafdca7bcdf449f161a0abd839ba502ac29d
Christian <JA.PODHI.AU.MOSSAR123@gmail.comm>
2023-11-06 15:49:05 -0300

    http://localhost:5000/...

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
-import 'dotenv/config'
-import express from 'express'
-import cors from 'cors'
-
-import { connection} from '../repository/connection.js';
+import 'dotenv/config';
+import express from 'express';
+import cors from 'cors';
 
 const server = express();
 server.use(cors());
